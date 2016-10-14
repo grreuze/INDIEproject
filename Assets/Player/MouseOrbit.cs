e9818e417d81940c2dbbfd499914dd2ba051ad18
grreuze <grreuze@gmail.com>
2016-10-14 10:33:02 +0200

    Orbit

---
+++ Assets/Player/MouseOrbit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
+public class MouseOrbit : MonoBehaviour {
+
+    public Transform target;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    // Use this for initialization
+    void Start() {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+    }
+
+    void LateUpdate() {
+        if (target) {
+            if (Input.GetMouseButton(1)) {
+                x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+                y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+            }
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+            distance = distance - Input.GetAxis("Mouse ScrollWheel") * 5;
+            
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            Vector3 position = rotation * negDistance + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max) {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
